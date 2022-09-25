633ce8cb646fc413451e2245010d60430548158b
Sebastian Kroczek <skroczek@wavecon.de>
2022-09-25 10:45:44 +0200

    Revert "Use AbortWithStatus if suitable"
    This reverts commit b5559e8aa5be93bbda1701695db367d0b5c15378.

--- pkg/router.go
+++ pkg/router.go
@@ -4,20 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func DefaultRouter(server *Server, handlers ...gin.HandlerFunc) *gin.Engine {
+func DefaultRouter(server *Server) *gin.Engine {
 	// Creates a gin router with default middleware:
 	// logger and recovery (crash-free) middleware
 	router := gin.Default()
 
-	group := router.Group("/", handlers...)
-
-	group.GET("/*path", server.GetHandler)
-	group.POST("/*path", server.PostHandler)
-	group.PUT("/*path", server.PutHandler)
-	group.DELETE("/*path", server.DeleteHandler)
-	group.PATCH("/*path", server.PatchHandler)
-	group.HEAD("/*path", server.HeadHandler)
-	group.OPTIONS("/*path", server.OptionsHandler)
-
+	router.GET("/*path", server.GetHandler)
+	router.POST("/*path", server.PostHandler)
+	router.PUT("/*path", server.PutHandler)
+	router.DELETE("/*path", server.DeleteHandler)
+	router.PATCH("/*path", server.PatchHandler)
+	router.HEAD("/*path", server.HeadHandler)
+	router.OPTIONS("/*path", server.OptionsHandler)
 	return router
 }
